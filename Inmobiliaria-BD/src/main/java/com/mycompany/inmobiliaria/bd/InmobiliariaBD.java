282bc27e922a34d7b8bf53428989af241efec815
Candela Naranjo <146892282+candeenaranjo@users.noreply.github.com>
2026-06-18 15:28:41 -0300

    Proyecto

---
+++ Inmobiliaria-BD/src/main/java/com/mycompany/inmobiliaria/bd/InmobiliariaBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inmobiliaria.bd;
+
+/**
+ *
+ * @author maria
+ */
+public class InmobiliariaBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
