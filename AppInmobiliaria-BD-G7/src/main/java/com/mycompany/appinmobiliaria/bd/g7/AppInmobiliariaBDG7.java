9e87c8d0ee6affe21d14dc32dd631585c09fbdf4
Candela Naranjo <146892282+candeenaranjo@users.noreply.github.com>
2026-06-18 14:08:43 -0300

    Proyecto

---
+++ AppInmobiliaria-BD-G7/src/main/java/com/mycompany/appinmobiliaria/bd/g7/AppInmobiliariaBDG7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appinmobiliaria.bd.g7;
+
+/**
+ *
+ * @author maria
+ */
+public class AppInmobiliariaBDG7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
