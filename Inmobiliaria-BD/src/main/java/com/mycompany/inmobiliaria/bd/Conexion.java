2d73720decd55359a2c1c2f966a3d94b84e2a67e
Candela Naranjo <146892282+candeenaranjo@users.noreply.github.com>
2026-06-18 15:53:02 -0300

    conexion base de de datos

--- Inmobiliaria-BD/src/main/java/com/mycompany/inmobiliaria/bd/Conexion.java
+++ Inmobiliaria-BD/src/main/java/com/mycompany/inmobiliaria/bd/Conexion.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.inmobiliaria.bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author maria
  */
 public class Conexion {
+      private static String url = "jdbc:mariadb://localhost:3306/dreamprop inmobiliaria";
+    private static String usuario = "root";
+    private static String password = "";
+    private static Connection conexion = null;
+
+    public Conexion(String url, String usuario, String pass){
+        this.url = url;
+        this.usuario = usuario;
+        this.password = pass;
+    }
     
+    public static Connection getConexion() {
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url, usuario, password);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar el driver: " + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error de conexión: " + ex.getMessage());
+            }
+        }
+        return conexion;
+    }
 }
